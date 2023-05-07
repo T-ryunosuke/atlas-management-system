d6bef28388c91f82e1535e7e0b86af2c37be76b9
tokunaga_ryunosuke <kira.toyp@gmail.com>
2023-05-07 22:23:10 +0900

    投稿表示の検索のための記述や投稿時、投稿編集時、メイン・サブカテゴリー追加、コメント投稿時のバリデーション

--- app/Http/Controllers/Authenticated/BulletinBoard/PostsController.php
+++ app/Http/Controllers/Authenticated/BulletinBoard/PostsController.php
@@ -13,6 +13,8 @@ use App\Models\Users\User;
 use App\Http\Requests\BulletinBoard\PostFormRequest;
 use Auth;
 
+use DB;
+
 class PostsController extends Controller
 {
     public function show(Request $request){
@@ -21,12 +23,13 @@ class PostsController extends Controller
         $like = new Like;
         $post_comment = new Post;
         if(!empty($request->keyword)){
+            $keyword = $request->keyword;
             $posts = Post::with('user', 'postComments')
-            ->where('post_title', 'like', '%'.$request->keyword.'%')
-            ->orWhere('post', 'like', '%'.$request->keyword.'%')->get();
+            ->where('post_title', 'like', '%'.$keyword.'%')
+            ->orWhere('post', 'like', '%'.$keyword.'%')->orWhereHas('subCategories',function ($q) use ($keyword) { $q->where('sub_category', $keyword);})->get();
         }else if($request->category_word){
             $sub_category = $request->category_word;
-            $posts = Post::with('user', 'postComments')->get();
+            $posts = Post::with('user', 'postComments')->whereHas('subCategories',function ($q) use ($sub_category) { $q->where('sub_category', $sub_category);})->get();
         }else if($request->like_posts){
             $likes = Auth::user()->likePostId()->get('like_post_id');
             $posts = Post::with('user', 'postComments')
@@ -49,15 +52,34 @@ class PostsController extends Controller
     }
 
     public function postCreate(PostFormRequest $request){
-        $post = Post::create([
-            'user_id' => Auth::id(),
-            'post_title' => $request->post_title,
-            'post' => $request->post_body
+        $request->validate([
+            'post_category_id' => 'required|max:150',
+            'post_title' => 'required|string|max:100',
+            'post_body' => 'required|string|max:5000'
         ]);
-        return redirect()->route('post.show');
+        DB::beginTransaction();
+        try{
+            $post_get = Post::create([
+                'user_id' => Auth::id(),
+                'post_title' => $request->post_title,
+                'post' => $request->post_body
+            ]);
+            $post = Post::findOrFail($post_get->id);
+            $sub_categories = $request->post_category_id;
+            $post->subCategories()->attach($sub_categories);
+            DB::commit();
+            return redirect()->route('post.show');
+        }catch(\Exception $e){
+            DB::rollback();
+            return redirect()->route('post.input');
+        }
     }
 
     public function postEdit(Request $request){
+        $request->validate([
+            'post_title' => 'required|string|max:100',
+            'post_body' => 'required|string|max:5000'
+        ]);
         Post::where('id', $request->post_id)->update([
             'post_title' => $request->post_title,
             'post' => $request->post_body,
@@ -70,11 +92,28 @@ class PostsController extends Controller
         return redirect()->route('post.show');
     }
     public function mainCategoryCreate(Request $request){
+        $request->validate([
+            'main_category_name' => 'required|string|max:100|unique:main_categories,main_category'
+        ]);
         MainCategory::create(['main_category' => $request->main_category_name]);
         return redirect()->route('post.input');
     }
 
+    public function subCategoryCreate(Request $request){
+        $request->validate([
+            'main_category_id' => 'required|exists:main_categories,id',
+            'sub_category_name' => 'required|string|max:100|unique:sub_categories,sub_category'
+        ]);
+        SubCategory::create([
+            'main_category_id' => $request->main_category_id,
+            'sub_category' => $request->sub_category_name]);
+        return redirect()->route('post.input');
+    }
+
     public function commentCreate(Request $request){
+        $request->validate([
+            'comment' => 'required|string|max:2500'
+        ]);
         PostComment::create([
             'post_id' => $request->post_id,
             'user_id' => Auth::id(),
